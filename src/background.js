e3810a20d6f29eeda16e5bf85e84e8637c5b32b2
Lennart <Lennart@bastl-instruments.com>
2017-09-18 17:07:19 +0200

    sixyknobs generating sysexstream
    Signed-off-by: Lennart <Lennart@bastl-instruments.com>

--- src/background.js
+++ src/background.js
@@ -1,54 +1,35 @@
-// This is main process of Electron, started as first thing when your
-// app starts. This script is running through entire life of your application.
-// It doesn't have any windows which you can see on screen, but we can open
-// window from here.
+'use strict';
 
-import path from 'path';
-import url from 'url';
-import { app, Menu } from 'electron';
-import { devMenuTemplate } from './menu/dev_menu_template';
-import { editMenuTemplate } from './menu/edit_menu_template';
-import createWindow from './helpers/window';
+var electron = require('electron');
 
-// Special module holding environment variables which you declared
-// in config/env_xxx.json file.
-import env from './env';
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
 
-const setApplicationMenu = () => {
-  const menus = [editMenuTemplate];
-  if (env.name !== 'production') {
-    menus.push(devMenuTemplate);
-  }
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-};
+var mainWindow = null;
 
-// Save userData in separate folders for each environment.
-// Thanks to this you can use production and development versions of the app
-// on same machine like those are two separate apps.
-if (env.name !== 'production') {
-  const userDataPath = app.getPath('userData');
-  app.setPath('userData', `${userDataPath} (${env.name})`);
-}
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 1200
+    });
+	
+    mainWindow.loadURL('file://' + __dirname + '/app.html');
 
-app.on('ready', () => {
-  setApplicationMenu();
-
-  const mainWindow = createWindow('main', {
-    width: 1000,
-    height: 600,
-  });
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'app.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
-
-  if (env.name === 'development') {
     mainWindow.openDevTools();
-  }
+
+    mainWindow.webContents.once('dom-ready', () => {
+	
+	var midi = require('midi');
+	var midiOut = new midi.output();
+
+	var midiPortList = [];
+	for (var i= 0; i<midiOut.getPortCount(); i++){
+		midiPortList.push(midiOut.getPortName(i));
+	}
+
+	mainWindow.webContents.send('midi_port_options', midiPortList);
+	});
 });
 
-app.on('window-all-closed', () => {
-  app.quit();
-});
+
+	
