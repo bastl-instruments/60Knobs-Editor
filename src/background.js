e9022fc504c37d42549e73493418b39b73215f1f
Lennart <Lennart@bastl-instruments.com>
2017-10-02 17:36:00 +0200

    sending midi works

--- src/background.js
+++ src/background.js
@@ -4,32 +4,43 @@ var electron = require('electron');
 
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+var midi = require('midi');
+var ipcMain = electron.ipcMain;
 
 var mainWindow = null;
+var midiOut = new midi.output();
+
+
+function sendMIDIPortOptions() {
+  var midiPortList = [];
+  for (var i= 0; i<midiOut.getPortCount(); i++){
+    midiPortList.push(midiOut.getPortName(i));
+  }
+	mainWindow.webContents.send('midi_port_options', midiPortList);
+}
+
+function sendMIDIData(data) {
+  midiOut.openPort(parseInt(data.port));
+  data.data.forEach(function(item, index) {
+    midiOut.sendMessage(item);
+  });
+  midiOut.closePort();
+}
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         height: 600,
         width: 1200
     });
-	
-    mainWindow.loadURL('file://' + __dirname + '/app.html');
 
+    mainWindow.loadURL('file://' + __dirname + '/app.html');
     mainWindow.openDevTools();
 
+    ipcMain.on('send_midi_data', function (event,message) {
+        sendMIDIData(message);
+    });
+
     mainWindow.webContents.once('dom-ready', () => {
-	
-	var midi = require('midi');
-	var midiOut = new midi.output();
-
-	var midiPortList = [];
-	for (var i= 0; i<midiOut.getPortCount(); i++){
-		midiPortList.push(midiOut.getPortName(i));
-	}
-
-	mainWindow.webContents.send('midi_port_options', midiPortList);
+      sendMIDIPortOptions();
 	});
 });
-
-
-	
