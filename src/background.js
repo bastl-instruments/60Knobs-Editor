602dd30defbd18496ad40054a6c44d135bd44519
Lennart <Lennart@bastl-instruments.com>
2017-11-14 09:34:01 +0100

    removed tests
    Signed-off-by: Lennart <Lennart@bastl-instruments.com>

--- src/background.js
+++ src/background.js
@@ -34,7 +34,7 @@ app.on('ready', function() {
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app.html');
-    mainWindow.openDevTools();
+    //mainWindow.openDevTools();
 
     ipcMain.on('send_midi_data', function (event,message) {
         sendMIDIData(message);
